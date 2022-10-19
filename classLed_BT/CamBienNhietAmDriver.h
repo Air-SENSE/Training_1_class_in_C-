bf393496c8f4b9f05c32b22e1e7c100073265506
ThuNguyen26 <nthu86958@gmail.com>
2022-10-19 20:25:57 +0700

    Update file CamBienNhietAmDriver.h

--- classLed_BT/CamBienNhietAmDriver.h
+++ classLed_BT/CamBienNhietAmDriver.h
@@ -1,31 +1,50 @@
 /* ý nghĩa: Điều hiển thị trên màn hình lcd 16x2 i2c
  * Người viết: Tuyền
- * cập nhật cuối: 07/07/2022 
- * 
+ * cập nhật cuối: 07/07/2022
+ *
  */
 #include <DHT.h>
 
 #ifdef DHT_SENSOR
-  DHT camBienNhietAm(DHT_PIN, DHT11);
+DHT CamBienNhietAm(DHT_PIN, DHT11);
 #endif
 #ifdef SHT_SENSOR
 //
 #endif
 
-void KhoiTaoNhietAm();
-int docNhietDo();
-int docDoAm();
+/**
+ * @brief Khoi tao cam bien nhiet do, do am
+ * @paramin None
+ * @paramout None
+ * @return None
+ */
+void KhoiTaoNhietAm_KhoiTao();
 
+/**
+ * @brief Doc gia tri nhiet do
+ * @paramin None
+ * @paramout None
+ * @return gia tri nhiet do
+ */
+int DocNhietDo_Doc();
 
-void KhoiTaoNhietAm()
+/**
+ * @brief Doc gia tri do am
+ * @paramin None
+ * @paramout None
+ * @return gia tri do am
+ */
+int DocDoAm_Doc();
+
+void KhoiTaoNhietAm_KhoiTao()
 {
-  camBienNhietAm.begin();
+  CamBienNhietAm.begin();
 }
-int docNhietDo()
+int DocNhietDo_Doc()
 {
   return (int)camBienNhietAm.readTemperature();
 }
-int docDoAm()
+int DocDoAm_Doc()
 {
-    return (int)camBienNhietAm.readHumidity();
+  return (int)camBienNhietAm.readHumidity();
 }
